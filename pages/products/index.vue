32c8fbeabb39b5b41f35956d6d2f300f5e80c3ca
Daniel Kelly <me@danielkelly.io>
2023-03-07 16:29:04 -0600

    3.1-end

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,9 +1,24 @@
 <script setup>
-import data from "@/data.json";
-const products = data.products;
+const route = useRoute();
+
+// handle skip
+const skip = ref(route.query.skip | 0);
+watch(skip, () => useRouter().push({ query: { skip: skip.value } }));
+
+// Fetch product data
+const perPage = 30;
+const { data, pending } = await useFetch(
+  () => `https://dummyjson.com/products?skip=${skip.value}&limit=${perPage}`,
+  { key: "products" }
+);
+const products = computed(() => data.value.products);
+
+// handle loading
+const pendingDebounced = refDebounced(pending, 1000);
 </script>
 <template>
-  <div>
+  <div v-if="pendingDebounced">loading...</div>
+  <div v-else>
     <ul>
       <li v-for="product in products" :key="product.id">
         <NuxtLink :to="`/products/${product.id}`"
@@ -12,8 +27,18 @@ const products = data.products;
       </li>
     </ul>
     <div class="opacity-50">
-      <a class="inline-block mr-3">Previous</a>
-      <a>Next</a>
+      <a
+        @click.prevent="skip = skip - perPage"
+        v-if="data.skip !== 0"
+        class="inline-block mr-3 cursor-pointer"
+        >Previous</a
+      >
+      <a
+        v-if="data.skip + perPage < data.total"
+        @click.prevent="skip = skip + perPage"
+        class="cursor-pointer"
+        >Next</a
+      >
     </div>
   </div>
 </template>
