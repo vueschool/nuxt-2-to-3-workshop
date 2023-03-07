d3919888af7abc476fea7f4052ff54e916d23bcc
Daniel Kelly <me@danielkelly.io>
2023-03-07 16:31:10 -0600

    3.2-end

---
+++ server/api/test/[id].get.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler((event) => {
+  return {
+    query: getQuery(event),
+    params: getRouterParams(event),
+    requestHeaders: getRequestHeaders(event),
+    method: getMethod(event),
+  };
+});
