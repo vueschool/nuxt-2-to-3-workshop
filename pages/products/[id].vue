9501651f5753ad67a6298b926599ea73cbd8016e
Daniel Kelly <me@danielkelly.io>
2023-03-07 16:15:32 -0600

    wip

---
+++ pages/products/[id].vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import data from "@/data.json";
+
+const id = computed(() => useRoute().params.id);
+
+const product = computed(() => {
+  return data.products.find((p) => p.id === Number(id.value));
+});
+</script>
+
+<template>
+  <pre>{{ product }}</pre>
+</template>
