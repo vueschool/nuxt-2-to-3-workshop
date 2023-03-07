ea942c0d239b984c59f0994184fc33095e639ec0
Daniel Kelly <me@danielkelly.io>
2023-03-07 16:53:47 -0600

    6.1 end

---
+++ pages/markdown.vue
@@ -0,0 +1,11 @@
+<script setup>
+const text = `
+# Hello world
+
+![iphone](https://i.dummyjson.com/data/products/2/1.jpg)
+
+`;
+</script>
+<template>
+  <Markdown :md="text"></Markdown>
+</template>
